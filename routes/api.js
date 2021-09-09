4f9a5942ef76b3fc1fc69a75d1b6a3b6fccdca98
Lance Bailey <lancebailey222@gmail.com>
2021-09-09 13:27:31 -0400

    added some html and api routes

---
+++ routes/api.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const Workout = require("../models/Workout.js");
+
+//create
+router.post("/api/Workout", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+//read
+router.get("/api/Workout", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
